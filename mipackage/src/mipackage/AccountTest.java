10e3679f4642c40627153196461601032fea5a62
usuario <usuario@LAPTOP-EBET0HUE>
2020-04-17 10:39:57 +0200

    version 1.0.0irst commit

---
+++ mipackage/src/mipackage/AccountTest.java
@@ -0,0 +1,22 @@
+package mipackage;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+	@Test
+	@DisplayName(value = "INGRESAR DINERO EN CUENTA")
+	void testIngresar() throws Exception {
+		Account Cantidad = new Account(10);
+		Cantidad.ingresar(150);
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	@DisplayName(value = "RETIRAR DINERO DE LA CUENTA")
+	void testRetirar() throws Exception {
+		Account Cantidad = new Account(10);
+		Cantidad.retirar(0);
+		// fail("Not yet implemented");
+	}
+}
